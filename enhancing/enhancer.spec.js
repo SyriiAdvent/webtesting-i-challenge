920fc6058c8c881ca1c25f7355b2524d1a637c23
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-22 13:29:50 -0700

    repair, succeeed, and fail all tested

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,56 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('Black Desert Enchanting System Test', () => {
+	describe('.repair()', () => {
+		test('should return the current object durability repair to 100', () => {
+			const kzarkaGauntlet = {
+				ap: 22,
+				durability: 76,
+				enhancement: 1,
+				crit: 7,
+				slots: 2,
+			};
+			expect(enhancer.repair(kzarkaGauntlet).durability).toBe(100);
+		});
+	});
+
+	describe('.succeed', () => {
+		const KaiaStaff = {
+			ap: 63,
+			durability: 76,
+			enhancement: 11,
+			crit: 14,
+			slots: 2,
+    };
+    const kzarkaStaff = {
+			ap: 22,
+			durability: 76,
+			enhancement: 20,
+			crit: 7,
+			slots: 2,
+		};
+		test('should return item enhancement + 1', () => {
+			expect(enhancer.succeed(KaiaStaff).enhancement).toBe(12);
+    });
+    test('should not enchant a item at 20 already', () => {
+			expect(enhancer.succeed(kzarkaStaff).enhancement).toBe(20);
+		});
+  });
+  
+  describe('.fail', () => {
+		const KaiaAmulet = {
+			ap: 63,
+			durability: 76,
+			enhancement: 11,
+			crit: 14,
+			slots: 2,
+		};
+		test('should return item enhancement - 1', () => {
+			expect(enhancer.fail(KaiaAmulet).enhancement).toBe(10);
+    });
+    test('should return durabiulity - 10', () => {
+			expect(enhancer.fail(KaiaAmulet).durability).toBe(71);
+		});
+	});
+});
